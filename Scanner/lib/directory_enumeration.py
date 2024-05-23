f35010c3b027c5b3bbd1a71492ffbce6336fdf9a
fledson.chagas <fledson.chagas@intelliway.com.br>
2024-05-23 20:23:15 -0300

    updating directory_enumeration.py

--- Scanner/lib/directory_enumeration.py
+++ Scanner/lib/directory_enumeration.py
@@ -1,40 +1,42 @@
 from .http_requests import fetch_url
-from threading import Thread
-from concurrent.futures import ThreadPoolExecutor
+from threading import Thread, Lock
 import requests
 from requests.exceptions import RequestException
 import os
 from tqdm import tqdm
-import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+lock = Lock()
 
-
-def load_seclists_paths():
-    seclists_path = '../SecLists/Discovery/Web-Content/common.txt'
-    if not os.path.exists(seclists_path):
-        raise FileNotFoundError(f"O arquivo 'common.txt' não foi encontrado em: {seclists_path}")
-    with open(seclists_path, 'r') as file:
+def load_common_paths():
+    directory_of_this_script = os.path.dirname(__file__)
+    path_to_common_paths = os.path.join(directory_of_this_script, 'seclists', 'Discovery', 'Web-Content', 'common.txt')
+    if not os.path.exists(path_to_common_paths):
+        raise FileNotFoundError(f"O arquivo 'common.txt' não foi encontrado em: {path_to_common_paths}")
+    with open(path_to_common_paths, 'r', encoding='latin-1') as file:  # Usando encoding para suportar caracteres especiais
         return [line.strip() for line in file if line.strip()]
 
-
-def check_path(url, results):
+def check_path(url, results, progress_bar):
     response = fetch_url(url)
     if response and response.status_code == 200:
-        results.append(url)
-
+        with lock:
+            results.append(url)
+    with lock:
+        progress_bar.update(1)
 
 def directory_enumeration(url):
-    common_paths = load_seclists_paths()
+    common_paths = load_common_paths()
     results = []
+    threads = []
     progress_bar = tqdm(total=len(common_paths), desc="Buscando diretórios expostos", unit="dir")
 
-    with ThreadPoolExecutor(max_workers=30) as executor:  # Limita a 10 threads simultâneas
-        futures = [executor.submit(check_path, f"{url}/{path}", results) for path in common_paths]
-        for future in futures:
-            future.result()
-            progress_bar.update(1)
+    for path in common_paths:
+        full_url = f"{url}/{path}"
+        thread = Thread(target=check_path, args=(full_url, results, progress_bar))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
 
     progress_bar.close()
     if results:
@@ -44,14 +46,12 @@ def directory_enumeration(url):
     else:
         print("\nNão há diretórios expostos.")
 
-
 def fetch_url(url):
-    session = requests.Session()
     if not url.startswith(('http://', 'https://')):
         url = 'http://' + url  # Assume http como padrão se nenhum esquema for fornecido
     try:
-        response = session.get(url, timeout=5)
+        response = requests.get(url, timeout=5)
         return response if response.ok else None
     except RequestException as e:
-        logger.error(f"Erro ao conectar a {url}: {e}")
+
         return None
